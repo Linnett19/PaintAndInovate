ba519a7576d2d007b4442af891f9a30ad03a3898
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2025-04-17 19:35:23 +0300

    fd

--- src/main/java/com/linnett/paint_and_inovate/Paint_and_inovate.java
+++ src/main/java/com/linnett/paint_and_inovate/Paint_and_inovate.java
@@ -7,6 +7,7 @@ import com.linnett.paint_and_inovate.common.item.ModCreativeTabs;
 import com.linnett.paint_and_inovate.common.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.util.Tuple;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -16,9 +17,16 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.util.thread.SidedThreadGroups;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Mod(Paint_and_inovate.MODID)
 public class Paint_and_inovate {
@@ -49,7 +57,23 @@ public class Paint_and_inovate {
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
     }
+    private static final Collection<Tuple<Runnable, Integer>> workQueue = new ConcurrentLinkedQueue<>();
 
+    public static void queueServerWork(int tick, Runnable action) {
+        if (Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER)
+            workQueue.add(new Tuple<>(action, tick));
+    }
+    @SubscribeEvent
+    public void tick(ServerTickEvent.Post event) {
+        List<Tuple<Runnable, Integer>> actions = new ArrayList<>();
+        workQueue.forEach(work -> {
+            work.setB(work.getB() - 1);
+            if (work.getB() == 0)
+                actions.add(work);
+        });
+        actions.forEach(e -> e.getA().run());
+        workQueue.removeAll(actions);
+    }
     @EventBusSubscriber(modid = "paint_and_inovate", bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public class ClientModEvents {
 
